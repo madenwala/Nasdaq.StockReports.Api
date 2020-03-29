af1bf052f7e6c5df936de95a0b074f30f15c6e4b
Mohammed Adenwala <adenwala@outlook.com>
2020-03-29 10:36:51 -0500

    Added properties for EarningsDate, IsEstimate, IsBeforeMarketOpen

--- src/Models/EarningsDateResponse.cs
+++ src/Models/EarningsDateResponse.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Nasdaq.StockReports.Api.Models
 {
@@ -16,7 +18,18 @@ namespace Nasdaq.StockReports.Api.Models
         {
             get
             {
-                return DateTime.Now;
+                if (!string.IsNullOrEmpty(this.reportText))
+                {
+                    var regex = new Regex(@"\d{2}\/\d{2}\/\d{4}");
+                    foreach (Match m in regex.Matches(this.reportText))
+                    {
+                        DateTime dt;
+                        if (DateTime.TryParseExact(m.Value, "MM/dd/yyyy", null, DateTimeStyles.None, out dt))
+                            return dt;
+                    }
+                }
+
+                return null;
             }
         }
 
@@ -24,7 +37,15 @@ namespace Nasdaq.StockReports.Api.Models
         {
             get
             {
-                return true;
+                return this.reportText?.IndexOf("estimated", StringComparison.InvariantCultureIgnoreCase) >= 0;
+            }
+        }
+
+        public bool IsBeforeMarketOpen
+        {
+            get
+            {
+                return this.reportText?.IndexOf("before market open", StringComparison.InvariantCultureIgnoreCase) >= 0;
             }
         }
     }
